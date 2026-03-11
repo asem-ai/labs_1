40b4edc59706ecd818246695d61e403de50b5f50
asem-ai <ajmahansem@gmail.com>
2026-03-11 21:46:02 +0500

    new change

--- lab3/3.py
+++ lab3/3.py
@@ -125,7 +125,6 @@ def is_prime(x):
         if x % i == 0:
             return False
     return True
-
 def special_numbers(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
@@ -138,7 +137,27 @@ def special_numbers(n):
             yield "простое"
         else:
             yield i
-
-# Пример вызова
 for x in special_numbers(15):
+    print(x)
+
+#2 esep
+words = ["кот", "машина", "арбуз", "дом", "ананас"]
+result = [
+    (lambda w:
+        w.upper() + "*" if len(w) > 4 and 'а' in w else
+        "short" if len(w) <= 4 else
+        w.upper()
+    )(word)
+    for word in words
+]
+print(result)
+
+#3 esep
+def process_numbers(numbers):
+    positive = filter(lambda x: x > 0, numbers)
+    transformed = map(lambda x: x / 2 if x % 2 == 0 else x * 3 + 1, positive)
+    for item in transformed:
+        yield item
+numbers = [5, -2, 8, 0, -7, 3]
+for x in process_numbers(numbers):
     print(x)
