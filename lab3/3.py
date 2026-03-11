4022c778f4d91415fa1ec77e3f4468fab538cc53
asem-ai <ajmahansem@gmail.com>
2026-03-11 14:44:40 +0500

    new task

--- lab3/3.py
+++ lab3/3.py
@@ -14,3 +14,7 @@ numbers = [5, 12, 7, 20, 33, 8]
 filtered = list(filter(lambda x: x > 10 and x % 2 == 0, numbers))
 print(filtered)
 
+#4 esep
+numbers = [1, 2, 3, 4, 5, 6]
+mapped = list(map(lambda x: x**2 if x % 2 == 0 else x * 3, numbers))
+print(mapped)
