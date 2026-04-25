db488842e7513ad085c0b1515d27bd417c8f9e78
asem-ai <ajmahansem@gmail.com>
2026-04-25 12:00:49 +0500

    1 task

--- lab6/6.py
+++ lab6/6.py
@@ -1,1 +1,10 @@
-#task 1
+#1 task
+import pandas as pd
+df = pd.read_excel('catalog_products.xlsx')
+print("Форма DataFrame:", df.shape)
+print("\nТиптері:")
+print(df.dtypes)
+print("\nПропусктер саны:")
+print(df.isnull().sum())
+print("\nАлғашқы 5 жол:")
+print(df.head())
